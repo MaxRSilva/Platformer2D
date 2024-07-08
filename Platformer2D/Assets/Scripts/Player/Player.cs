217d797927f1c057a1389f4d94fba7306cd5fbcc
Max Ramon <MaxRamonNB@Gmail.com>
2024-07-08 14:40:32 -0300

    add animation on player

--- Platformer2D/Assets/Scripts/Player/Player.cs
+++ Platformer2D/Assets/Scripts/Player/Player.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 
 {
     public Rigidbody2D myRigidbody;
+    
+    [Header("Speed Setup")]
     public Vector2 friction = new Vector2(.1f, 0 );
     public float speed;
     public float forcejump = 2;
     public float speedRun;
+
+    [Header("Animation Setup")]
+
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.7f;
+    public float DownScaleX = -0.7f;
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
 
     private float _currentSpeed;
 
@@ -51,9 +62,36 @@ public class Player : MonoBehaviour
     }
     private void HandleJump()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) 
-        myRigidbody.velocity = Vector2.up * forcejump;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          myRigidbody.velocity = Vector2.up * forcejump;
+          myRigidbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidbody.transform);
+            Handlescalejump();
+                   
+        
+        }
     }
 
+    private void Handlescalejump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2,LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+
+       
+
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+       myRigidbody.transform.DOScaleX(DownScaleX, animationDuration).SetLoops(1, LoopType.Yoyo).SetEase(ease);
+        
+    }*/
     
+    
+
 }
+
+    
