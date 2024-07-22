dd8310cf8ba464f17e6df1a1a72560ea883ed941
Max Ramon <MaxRamonNB@Gmail.com>
2024-07-22 20:29:04 -0300

    update coins count

--- Platformer2D/Assets/Scripts/Colectable/ItemManager.cs
+++ Platformer2D/Assets/Scripts/Colectable/ItemManager.cs
@@ -5,15 +5,26 @@ using Core.Singleton;
 
 public class ItemManager : Singleton<ItemManager>
 {
-   
+    public static ItemManager Instance;
     public int coins;
 
     
 
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+        
+    }
+
     private void Start()
     {
         Reset();
+        
     }
+
     private void Reset()
     {
         coins = 0;  
