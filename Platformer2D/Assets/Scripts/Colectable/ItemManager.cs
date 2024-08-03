d67d3dddec1f982e3641ad3f662a901a317589a5
Max Ramon <MaxRamonNB@Gmail.com>
2024-08-03 11:41:17 -0300

    update UI in classic mode

--- Platformer2D/Assets/Scripts/Colectable/ItemManager.cs
+++ Platformer2D/Assets/Scripts/Colectable/ItemManager.cs
@@ -8,8 +8,8 @@ public class ItemManager : Singleton<ItemManager>
 {
     public static ItemManager Instance;
     public int coins;
-    public TMP_Text coinsText;
-
+    //public TMP_Text coinsText;
+    public TextMeshProUGUI uiTextCoin;
     
 
     private void Awake()
@@ -29,13 +29,22 @@ public class ItemManager : Singleton<ItemManager>
 
     private void Reset()
     {
-        coins = 0;  
+        coins = 0;
+        UpdateUI();
         
     }
 
     public void AddCoins(int amount = 1)
     {
         coins+= amount;
-        coinsText.text = coins.ToString();
+        //coinsText.text = coins.ToString();
+        UpdateUI();
     }
+
+    public void UpdateUI()
+    {
+       uiTextCoin.text = coins.ToString();
+    }
+
+
 }
